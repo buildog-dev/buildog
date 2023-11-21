b5ba40f1ae31c133bca93dc6ba8b8c31942a31d8
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-11-21 03:34:19 +0300

    Create Welcome Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { usePathname, useRouter } from "next/navigation";
 export default function Page() {
-  return <div> Welcoming </div>;
+  const pathname = usePathname();
+  const router = useRouter();
+
+  return (
+    <div className="mx-auto flex  justify-center items-center min-h-screen">
+      <div className="mx-auto flex  flex-col items-center justify-center space-y-6 sm:w-[350px]">
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-4xl font-semibold tracking-tight">
+            Welcoming Page
+          </h1>
+        </div>
+        <Button
+          className="min-w-[19rem]"
+          onClick={() => router.push("/login/")}
+        >
+          Sign in
+        </Button>
+      </div>
+    </div>
+  );
 }
