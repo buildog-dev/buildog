c3cd0f34004e59e210600ee57db6c4eb41294176
Yuanda1 <emirbgrck0@gmail.com>
2023-11-22 22:40:55 +0300

    Add delete button on blog card

--- src/app/(blog-pages)/blog/page.tsx
+++ src/app/(blog-pages)/blog/page.tsx
@@ -3,6 +3,17 @@
 import React, { useEffect, useState } from "react";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { ResetIcon } from '@radix-ui/react-icons'
+import { Cross1Icon } from '@radix-ui/react-icons'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button";
 
 type Blog = {
   cards: {
@@ -14,20 +25,33 @@ type Blog = {
   text: string;
 };
 
-type Blogs = {
-  [filename: string]: Blog;
-};
 
 export default function Page() {
-  const [blogs, setBlogs] = useState<Blogs>();
+  const [blogs, setBlogs] = useState<Blog[]>();
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     const localStorageBlogs = JSON.parse(
       localStorage.getItem("blogs") as string
     );
     setBlogs(localStorageBlogs);
+    
   }, []);
 
+
+const deleteBlog = (filenameToDelete: string) => {
+    setOpen(!open)
+    if (blogs) {
+      for (const el in blogs) {
+        if (blogs[el].filename === filenameToDelete) {
+          delete blogs[el];
+        }
+      }
+    }
+
+    localStorage.setItem("blogs", (JSON.stringify(blogs)));
+};
+  
   return (
     <div className="space-y-4">
       <Input />
@@ -37,8 +61,33 @@ export default function Page() {
             <Card key={key}>
               <CardHeader>
                 <CardTitle>{blog.filename}</CardTitle>
+                <Button variant="destructive" className="w-10 ml-auto" onClick={() => setOpen(!open)} size="sm">
+                   <Cross1Icon />
+                  </Button>
                 {/* <CardDescription>{blog}</CardDescription> */}
               </CardHeader>
+              <Dialog onOpenChange={setOpen} open={open}>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>Delete Blog</DialogTitle>
+ 
+          <DialogDescription>
+                      Are you sure for delete this blog?
+                    </DialogDescription>
+                  </DialogHeader>
+                  <DialogFooter>
+                    <Button variant="ghost" onClick={() => setOpen(!open)}>
+                      No
+                    </Button>
+                    <Button
+                      variant="destructive"
+                      onClick={() => deleteBlog(blog.filename)}
+                    >
+                      Yes
+                    </Button>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
             </Card>
           ))
         ) : (
