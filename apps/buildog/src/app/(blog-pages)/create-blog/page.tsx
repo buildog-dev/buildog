520d536f38049bfc8c1cbc3c7089de6bfb964552
Osman Coskun <osman.coskun95@gmail.com>
2024-01-31 01:56:59 +0300

    refactor: page refactored with new design.

--- apps/buildog/src/app/(blog-pages)/create-blog/page.tsx
+++ apps/buildog/src/app/(blog-pages)/create-blog/page.tsx
@@ -1,68 +1,40 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Markdown from "react-markdown";
 import { Button } from "@ui/components/button";
 import { Input } from "@ui/components/input";
-import DragDropList, { Item } from "@/components/drag-drop-list";
+import { Textarea } from "@ui/components/textarea";
+import { Label } from "@ui/components/label";
+import { Tabs, TabsList, TabsTrigger } from "@ui/components/ui/tabs"
+
 import remarkGfm from "remark-gfm";
-import { create_UUID } from "@/lib/uuid";
-import { useSearchParams } from "next/navigation";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@ui/components/dialog";
 
 export default function Page() {
   const [filename, setFilename] = useState<string>();
-  const [cards, setCards] = useState<Item[]>([]);
-  const query = useSearchParams();
+  const [tab, setTab] = useState<string>("split")
+  const [markdownContent, setMarkdownContent] = useState<string>("");
 
-  useEffect(() => {
-    const qsFilename = query.get("filename");
-    if (qsFilename) {
-      const localStorageBlogs = JSON.parse(
-        localStorage.getItem("blogs") as string
-      );
+  const filenameHandler = (e) => {
+    setFilename(e.target.value)
+  }
 
-      const blog = localStorageBlogs[qsFilename];
-      setFilename(blog.filename);
-      setCards(blog.cards);
-    } else {
-      // This is for initial markdown
-      addMarkdownArea();
-    }
-  }, []);
-
-  const addMarkdownArea = () => {
-    setCards((prevCards) => {
-      return [
-        ...prevCards,
-        {
-          id: create_UUID(),
-          value: "",
-          lock: false,
-        },
-      ];
-    });
-  };
+  const tabChangeHandler = (value) => {
+    setTab(value)
+  }
+  const markdownInputHandler = (e) => {
+    setMarkdownContent(e.target.value)
+  }
 
   const save = () => {
-    if (filename && cards.length > 0) {
-      const text = cards?.map((item) => item.value + "\n\n").join("");
-
+    if (filename && markdownContent.length > 0) {
       const currentLocalStorage = JSON.parse(
         localStorage.getItem("blogs") as string
       );
 
       const newLocalStorage = {
         ...currentLocalStorage,
-        [filename]: { cards, filename, text },
+        [filename]: { filename, markdownContent },
       };
 
       localStorage.setItem("blogs", JSON.stringify(newLocalStorage));
@@ -72,67 +44,40 @@ export default function Page() {
   };
 
   return (
-    <div className="h-full flex flex-col gap-4">
-      <div className="flex items-center justify-between p-2 border rounded-lg w-full">
-        <Input
-          onChange={(e) => setFilename(e.target.value)}
-          value={filename}
-          placeholder="Filename"
-          className="w-[400px]"
-        />
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button>Preview & Save</Button>
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-[705px]">
-            <DialogHeader>
-              <DialogTitle>Preview</DialogTitle>
-            </DialogHeader>
-            <div className="h-[500px] overflow-y-auto w-full p-5 border rounded-lg space-y-4">
-              <article className="prose dark:prose-invert">
-                <Markdown remarkPlugins={[remarkGfm]}>
-                  {cards?.map((item) => item.value + "\n\n").join("")}
-                </Markdown>
-              </article>
-            </div>
-            <DialogFooter>
-              <DialogClose asChild>
-                <Button className="w-full" variant="destructive">
-                  Cancel
-                </Button>
-              </DialogClose>
-              <Button onClick={save} className="w-full">
-                Save changes
-              </Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-      </div>
-      <div className="flex h-full gap-6">
-        <div className="w-full space-y-4">
-          <div className="w-full p-5 border rounded-lg space-y-4">
-            <Button
-              onClick={addMarkdownArea}
-              size="sm"
-              className="text-end"
-              variant="secondary"
-            >
-              Add Markdown Area
-            </Button>
-            <div className="max-h-[calc(100vh_-_200px)] overflow-y-auto">
-              <DragDropList cards={cards} setCards={setCards} />
-            </div>
+    <>
+      <div className="flex gap-5 mb-5">
+        <div className="w-full">
+          {tab == "editor" ? <Textarea id="markdownContent" className="h-96" onInput={markdownInputHandler} value={markdownContent} placeholder="Enter your blog content" /> :
+            tab == "preview" ? <Markdown className="w-full h-full border rounded-md py-1 px-2" remarkPlugins={[remarkGfm]}>{markdownContent}</Markdown> :
+              <div className="flex gap-5 h-96">
+                <Textarea
+                  id="markdownContent"
+                  className="h-96 w-full"
+                  onInput={markdownInputHandler}
+                  value={markdownContent}
+                  placeholder="Enter your blog content" />
+                <Markdown className="w-full border rounded-md py-1 px-2" remarkPlugins={[remarkGfm]}>{markdownContent}</Markdown>
+              </div>}
+        </div>
+        <div className="space-y-5">
+          <div className="">
+            <Label htmlFor="markdownContent">Filename</Label>
+            <Input onInput={filenameHandler} placeholder="Enter Filename" />
+          </div>
+          <div>
+            <Label className="">Markdown</Label>
+            <Tabs value={tab} onValueChange={tabChangeHandler} className="w-full mx-auto">
+              <TabsList>
+                <TabsTrigger value="editor">Editor</TabsTrigger>
+                <TabsTrigger value="split">Split</TabsTrigger>
+                <TabsTrigger value="preview">Preview</TabsTrigger>
+              </TabsList>
+            </Tabs>
           </div>
         </div>
 
-        <div className="overflow-y-auto w-full p-5 border rounded-lg space-y-4">
-          <article className="prose dark:prose-invert">
-            <Markdown remarkPlugins={[remarkGfm]}>
-              {cards?.map((item) => item.value + "\n\n").join("")}
-            </Markdown>
-          </article>
-        </div>
       </div>
-    </div>
+      <Button onClick={() => save()}>Save</Button>
+    </>
   );
 }
