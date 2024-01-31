0c5e9e3630538a108b58f3c105aef82b5102d3f4
Osman Coskun <osman.coskun95@gmail.com>
2024-02-01 01:53:30 +0300

    fix format

--- apps/buildog/src/app/(blog-pages)/create-blog/page.tsx
+++ apps/buildog/src/app/(blog-pages)/create-blog/page.tsx
@@ -21,6 +21,7 @@ export default function Page() {
 
   const tabChangeHandler = (value) => {
     setTab(value);
+    console.log(value);
   };
   const markdownInputHandler = (e) => {
     setMarkdownContent(e.target.value);
