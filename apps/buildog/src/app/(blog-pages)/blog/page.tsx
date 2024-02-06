edca415a9c8b3fea0edc421d6f17029c49212f66
erentaskiran <101654590+erentaskiran@users.noreply.github.com>
2024-02-06 10:12:54 +0300

    fix

--- apps/buildog/src/app/(blog-pages)/blog/page.tsx
+++ apps/buildog/src/app/(blog-pages)/blog/page.tsx
@@ -50,64 +50,64 @@ export default function Page() {
     });
   };
 
-  const createData = (blog: Blog) => {
-    return {
-      blogId: "",
-      fileName: blog.filename,
-      tags: "",
-    };
+  const tableData = () => {
+    return Object.values(blogs || {}).map((blog) => {
+      return {
+        blogId: "",
+        fileName: blog.filename,
+        tags: "",
+      };
+    });
   };
+
+  const columns = [
+    {
+      id: "fileName",
+      accessorKey: "fileName",
+      header: "Blog Name",
+    },
+    {
+      accessorKey: "blogId",
+      header: "Blog Id",
+    },
+    {
+      accessorKey: "tags",
+      header: "Tags",
+    },
+    {
+      accessorKey: "actions",
+      header: "Actions",
+      id: "actions",
+      cell: ({ row }) => {
+        const fileName = row.original.fileName;
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                ...
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem>Edit</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                onClick={() => {
+                  deleteBlog(fileName);
+                }}
+              >
+                Delete
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      },
+    },
+  ];
 
   return (
     <div className="space-y-4">
-      <DataTable
-        columns={[
-          {
-            id: "fileName",
-            accessorKey: "fileName",
-            header: "Blog Name",
-          },
-          {
-            accessorKey: "blogId",
-            header: "Blog Id",
-          },
-          {
-            accessorKey: "tags",
-            header: "Tags",
-          },
-          {
-            accessorKey: "actions",
-            header: "Actions",
-            id: "actions",
-            cell: ({ row }) => {
-              const fileName = row.original.fileName;
-              return (
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" className="h-8 w-8 p-0">
-                      ...
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                    <DropdownMenuItem>Edit</DropdownMenuItem>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem
-                      onClick={() => {
-                        deleteBlog(fileName);
-                      }}
-                    >
-                      Delete
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              );
-            },
-          },
-        ]}
-        data={Object.values(blogs || {}).map(createData)}
-        filterColumnId={"fileName"}
-      />
+      <DataTable columns={columns} data={tableData()} filterColumnId={"fileName"} />
     </div>
   );
 }
