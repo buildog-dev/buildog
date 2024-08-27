e916f5f1c4a69b4043494dd6130f363bbba6185a
Furkan Ünsalan <me@furkanunsalan.dev>
2024-08-27 15:25:32 +0300

    feat(buildog): add status and slug column to blog table (#63)
    * edited blogs table
    
    * edited blogs table
    
    * resolved the requests
    
    * update gitignore

--- packages/ui/components/ui/data-table.tsx
+++ packages/ui/components/ui/data-table.tsx
@@ -6,7 +6,9 @@ import {
   getCoreRowModel,
   useReactTable,
   getFilteredRowModel,
+  getSortedRowModel,
   ColumnFiltersState,
+  SortingState,
 } from "@tanstack/react-table";
 
 import {
@@ -19,6 +21,14 @@ import {
 } from "@ui/components/ui/table";
 
 import { Input } from "@ui/components/ui/input";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@ui/components/ui/dropdown-menu";
 
 import * as React from "react";
 
@@ -34,16 +44,27 @@ export function DataTable<TData, TValue>({
   filterColumnId,
 }: DataTableProps<TData, TValue>) {
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [selectedColumn, setSelectedColumn] = React.useState<string | null>(null);
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
-    onColumnFiltersChange: setColumnFilters,
     getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
     state: {
       columnFilters,
+      sorting,
     },
+    onColumnFiltersChange: setColumnFilters,
+    onSortingChange: setSorting,
   });
+
+  const handleSort = (columnId: string, direction: "asc" | "desc") => {
+    setSorting([{ id: columnId, desc: direction === "desc" }]);
+    setSelectedColumn(null); // Close dropdown after selection
+  };
 
   return (
     <div>
@@ -63,9 +84,83 @@ export function DataTable<TData, TValue>({
                 {headerGroup.headers.map((header) => {
                   return (
                     <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(header.column.columnDef.header, header.getContext())}
+                      {header.isPlaceholder ? null : (
+                        <DropdownMenu>
+                          <DropdownMenuTrigger asChild>
+                            <div
+                              onClick={() => setSelectedColumn(header.id)}
+                              className="cursor-pointer flex items-center"
+                            >
+                              {flexRender(header.column.columnDef.header, header.getContext())}
+                              {header.column.getIsSorted() ? (
+                                header.column.getIsSorted() === "desc" ? (
+                                  <svg
+                                    width="15"
+                                    height="15"
+                                    viewBox="0 0 15 15"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="ml-2 h-4 w-4"
+                                  >
+                                    <path
+                                      d="M7.5 2C7.77614 2 8 2.22386 8 2.5L8 11.2929L11.1464 8.14645C11.3417 7.95118 11.6583 7.95118 11.8536 8.14645C12.0488 8.34171 12.0488 8.65829 11.8536 8.85355L7.85355 12.8536C7.75979 12.9473 7.63261 13 7.5 13C7.36739 13 7.24021 12.9473 7.14645 12.8536L3.14645 8.85355C2.95118 8.65829 2.95118 8.34171 3.14645 8.14645C3.34171 7.95118 3.65829 7.95118 3.85355 8.14645L7 11.2929L7 2.5C7 2.22386 7.22386 2 7.5 2Z"
+                                      fill="currentColor"
+                                      fillRule="evenodd"
+                                      clipRule="evenodd"
+                                    ></path>
+                                  </svg>
+                                ) : (
+                                  <svg
+                                    width="15"
+                                    height="15"
+                                    viewBox="0 0 15 15"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="ml-2 h-4 w-4"
+                                  >
+                                    <path
+                                      d="M7.14645 2.14645C7.34171 1.95118 7.65829 1.95118 7.85355 2.14645L11.8536 6.14645C12.0488 6.34171 12.0488 6.65829 11.8536 6.85355C11.6583 7.04882 11.3417 7.04882 11.1464 6.85355L8 3.70711L8 12.5C8 12.7761 7.77614 13 7.5 13C7.22386 13 7 12.7761 7 12.5L7 3.70711L3.85355 6.85355C3.65829 7.04882 3.34171 7.04882 3.14645 6.85355C2.95118 6.65829 2.95118 6.34171 3.14645 6.14645L7.14645 2.14645Z"
+                                      fill="currentColor"
+                                      fillRule="evenodd"
+                                      clipRule="evenodd"
+                                    ></path>
+                                  </svg>
+                                )
+                              ) : (
+                                <svg
+                                  width="15"
+                                  height="15"
+                                  viewBox="0 0 15 15"
+                                  fill="none"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  className="ml-2 h-4 w-4"
+                                >
+                                  <path
+                                    d="M4.93179 5.43179C4.75605 5.60753 4.75605 5.89245 4.93179 6.06819C5.10753 6.24392 5.39245 6.24392 5.56819 6.06819L7.49999 4.13638L9.43179 6.06819C9.60753 6.24392 9.89245 6.24392 10.0682 6.06819C10.2439 5.89245 10.2439 5.60753 10.0682 5.43179L7.81819 3.18179C7.73379 3.0974 7.61933 3.04999 7.49999 3.04999C7.38064 3.04999 7.26618 3.0974 7.18179 3.18179L4.93179 5.43179ZM10.0682 9.56819C10.2439 9.39245 10.2439 9.10753 10.0682 8.93179C9.89245 8.75606 9.60753 8.75606 9.43179 8.93179L7.49999 10.8636L5.56819 8.93179C5.39245 8.75606 5.10753 8.75606 4.93179 8.93179C4.75605 9.10753 4.75605 9.39245 4.93179 9.56819L7.18179 11.8182C7.35753 11.9939 7.64245 11.9939 7.81819 11.8182L10.0682 9.56819Z"
+                                    fill="currentColor"
+                                    fillRule="evenodd"
+                                    clipRule="evenodd"
+                                  ></path>
+                                </svg>
+                              )}
+                            </div>
+                          </DropdownMenuTrigger>
+                          {selectedColumn === header.id && (
+                            <DropdownMenuContent align="start">
+                              <DropdownMenuLabel>
+                                Sort by{" "}
+                                {flexRender(header.column.columnDef.header, header.getContext())}
+                              </DropdownMenuLabel>
+                              <DropdownMenuItem onClick={() => handleSort(header.id, "asc")}>
+                                Ascending
+                              </DropdownMenuItem>
+                              <DropdownMenuItem onClick={() => handleSort(header.id, "desc")}>
+                                Descending
+                              </DropdownMenuItem>
+                            </DropdownMenuContent>
+                          )}
+                        </DropdownMenu>
+                      )}
                     </TableHead>
                   );
                 })}
