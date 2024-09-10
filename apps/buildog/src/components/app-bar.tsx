a3ef400d4a043e5a6707221955ead37fa08d8183
REDLANTERNDEV <87618221+REDLANTERNDEV@users.noreply.github.com>
2024-09-10 03:08:45 +0300

    feat(buildog): settings page for user (#85)
    * fix(buildog): route groups for login and signup (#78)
    
    * chore: Updated AuthLayout and removed some unnecessary imports
    
    * feat(buildog): settings page for user (#82)
    
    * feat(buildog): added account settings page and layout components

--- apps/buildog/src/components/app-bar.tsx
+++ apps/buildog/src/components/app-bar.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import React from "react";
+import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@ui/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
+  DropdownMenuSeparator,
 } from "@ui/components/ui/dropdown-menu";
+import { ExitIcon, GearIcon } from "@ui/components/react-icons";
 import { Auth } from "@/web-sdk";
 
 const Appbar = () => {
@@ -26,9 +29,16 @@ const Appbar = () => {
             </Avatar>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end" className="min-w-[200px]">
-            <DropdownMenuItem className="cursor-pointer">Profile</DropdownMenuItem>
-            <DropdownMenuItem className="cursor-pointer" onClick={handleLogout}>
-              Logout
+            <Link href="/account/settings">
+              <DropdownMenuItem className="cursor-pointer">
+                <GearIcon className="mr-1 h-4 w-4" />
+                <span>Settings</span>
+              </DropdownMenuItem>
+            </Link>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="cursor-pointer text-red-600" onClick={handleLogout}>
+              <ExitIcon className="mr-1 h-4 w-4" />
+              <span>Logout</span>
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
