e78a8ecd74d6ebd870e73b0bd464b09084678e46
efesavci <efesavci12@gmail.com>
2024-09-13 01:57:16 +0300

    sign in & sign up buttons created incase the user already has an account or not.

--- apps/buildog/src/app/(auth)/login/page.tsx
+++ apps/buildog/src/app/(auth)/login/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import { UserAuthForm } from "@/components/user-auth-form";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Authentication",
@@ -14,6 +15,12 @@ export default function AuthenticationPage() {
         <p className="text-sm text-muted-foreground">Enter your email below to sign in</p>
       </div>
       <UserAuthForm />
+      <div className="flex gap-1 items-center justify-center w-full text-sm text-primary">
+        <p> Don't have an account?</p>
+        <Link href="/signup" className="underline">
+          Sign Up
+        </Link>
+      </div>
     </>
   );
 }
