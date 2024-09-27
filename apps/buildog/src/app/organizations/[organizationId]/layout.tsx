0d57007f9b4dce588a647c0e3f5515c257522699
Furkan Ünsalan <me@furkanunsalan.dev>
2024-09-27 09:10:47 +0300

    feat(api): added a checker for organizations on layout

--- apps/buildog/src/app/organizations/[organizationId]/layout.tsx
+++ apps/buildog/src/app/organizations/[organizationId]/layout.tsx
@@ -1,5 +1,11 @@
+'use client'
+
 import Appbar from "@/components/app-bar";
+import { useCallback, useState, useEffect } from "react";
 import { Sidebar } from "@/components/sidebar";
+import { Service } from "@/web-sdk";
+import { useAuth } from "@/components/auth-provider";
+import { useRouter } from "next/navigation";
 
 export default function Layout({
   children,
@@ -8,7 +14,31 @@ export default function Layout({
   children: React.ReactNode;
   params: { organizationId: string };
 }) {
+  const { user } = useAuth();
+  const [organizations, setOrganizations] = useState([]);
   const { organizationId } = params;
+  const router = useRouter();
+
+  const fetchOrganizations = useCallback(async () => {
+    const response = await Service.makeAuthenticatedRequest("organizations");
+    if (response) {
+      setOrganizations(response);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!user) return;
+    fetchOrganizations();
+  }, [user, fetchOrganizations]);
+
+  useEffect(() => {
+    if (organizations.length === 0) return;
+
+    const orgExists = organizations.some((org) => org.OrganizationId === organizationId);
+    if (!orgExists) {
+      router.replace("/not-found");
+    }
+  }, [organizations, organizationId, router]);
 
   return (
     <div className="flex w-full">
