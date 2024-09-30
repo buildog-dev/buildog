e9420c33d6f106922d11fc11434b4b984d72f443
Furkan Ünsalan <me@furkanunsalan.dev>
2024-09-30 04:09:10 +0300

    feat(buildog): add header under /organizations page (#110)
    * feat(buildog): added org header and fixed cards
    
    * feat(buildog): fixed indent
    
    * fix(buildog): resolved issues
    
    * fix(buildog): resolved issues
    
    * Removed a file from pull request
    
    * fix(buildog): edited card shadows

--- apps/buildog/src/components/app-bar.tsx
+++ apps/buildog/src/components/app-bar.tsx
@@ -1,49 +1,16 @@
 "use client";
 
 import React from "react";
-import Link from "next/link";
-import { Avatar, AvatarFallback, AvatarImage } from "@ui/components/ui/avatar";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-  DropdownMenuSeparator,
-} from "@ui/components/ui/dropdown-menu";
-import { ExitIcon, GearIcon } from "@ui/components/react-icons";
-import { Auth } from "@/web-sdk";
+
 import OrgNavigation from "./org-navigation";
+import AvatarDropdown from "./avatar-dropdown";
 
 const Appbar = () => {
-  const handleLogout = () => {
-    Auth.signOut();
-  };
-
   return (
     <header className="px-4 border-b">
       <nav className="w-full flex items-center justify-between py-2">
         <OrgNavigation />
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <Avatar className="w-8 h-8">
-              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-              <AvatarFallback>JD</AvatarFallback>
-            </Avatar>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end" className="min-w-[200px]">
-            <Link href="/account/settings">
-              <DropdownMenuItem className="cursor-pointer">
-                <GearIcon className="mr-1 h-4 w-4" />
-                <span>Settings</span>
-              </DropdownMenuItem>
-            </Link>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="cursor-pointer text-red-600" onClick={handleLogout}>
-              <ExitIcon className="mr-1 h-4 w-4" />
-              <span>Logout</span>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <AvatarDropdown />
       </nav>
     </header>
   );
