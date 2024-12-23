3c1711deaa1a01721ba86d1aad124efc59a8b57f
Kuzey Kose <kosekuzey97@gmail.com>
2024-12-23 18:51:27 -0500

    Revert "feat(buildog): add sidebar components and mobile detection hook"
    This reverts commit cf33abcf0da251e97391e09855d09b61e0f9c194.

--- apps/buildog/src/app/account/layout.tsx
+++ apps/buildog/src/app/account/layout.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import Appbar from "@/components/app-bar";
-import { SidebarProvider } from "@ui/components/ui/sidebar";
 import { AccountSidebar } from "@/components/account-sidebar";
 
 export default function AccountLayout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-      <div className="flex w-full">
-        <AccountSidebar />
-        <div className="flex flex-col w-full">
-          <Appbar />
-          <main className="flex-grow p-5 mx-auto overflow-auto w-full">{children}</main>
-        </div>
+    <div className="flex w-full">
+      <AccountSidebar className="w-[300px] border-r" />
+      <div className="flex flex-col w-full">
+        <Appbar />
+        <div className="flex-grow p-5 mx-auto overflow-auto w-full">{children}</div>
       </div>
-    </SidebarProvider>
+    </div>
   );
 }
