a50668e07a0cf62f1a64a9bd3e092d0dc78ae9b5
REDLANTERNDEV <87618221+REDLANTERNDEV@users.noreply.github.com>
2024-12-23 17:36:20 +0300

    feat(buildog): add sidebar components and mobile detection hook

--- apps/buildog/src/components/sidebar.tsx
+++ apps/buildog/src/components/sidebar.tsx
@@ -1,15 +1,23 @@
 "use client";
 
 import { cn } from "@repo/ui/lib/utils";
-import { Button } from "@ui/components/button";
 import { usePathname, useRouter } from "next/navigation";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarMenu,
+  SidebarGroupContent,
+  SidebarMenuButton,
+} from "@ui/components/ui/sidebar";
+import { Pencil2Icon, FrameIcon, GlobeIcon, GearIcon } from "@ui/components/react-icons";
 
 interface SidebarProps {
   className?: string;
-  organizationId: string; // Accept organizationName as a prop
+  organizationId: string;
 }
 
-export function Sidebar({ className, organizationId }: SidebarProps) {
+export function AppSidebar({ className, organizationId }: SidebarProps) {
   const pathname = usePathname();
   const router = useRouter();
 
@@ -18,25 +26,25 @@ export function Sidebar({ className, organizationId }: SidebarProps) {
       children: [
         {
           name: "Blog",
-          icon: "",
-          route: `/organizations/${organizationId}/blog`, // Dynamic route
+          icon: <FrameIcon className="mr-1 h-4 w-4" />,
+          route: `/organizations/${organizationId}/blog`,
           key: "main-blog",
         },
         {
           name: "Create Blog",
-          icon: "",
+          icon: <Pencil2Icon className="mr-1 h-4 w-4" />,
           route: `/organizations/${organizationId}/create-blog`,
           key: "main-create-blog",
         },
         {
           name: "Web",
-          icon: "",
+          icon: <GlobeIcon className="mr-1 h-4 w-4" />,
           route: `/organizations/${organizationId}/web`,
           key: "main-web",
         },
         {
           name: "Settings",
-          icon: "",
+          icon: <GearIcon className="mr-1 h-4 w-4" />,
           route: `/organizations/${organizationId}/settings`,
           key: "main-settings",
         },
@@ -48,33 +56,43 @@ export function Sidebar({ className, organizationId }: SidebarProps) {
   const normalizePath = (path: string) => path.replace(/\/$/, "");
 
   return (
-    <div className={cn("pb-12", className)}>
-      <div className="space-y-4 py-4">
-        <div className="px-3 py-2">
-          {Object.entries(routes).map(([key, route]) => (
-            <div key={key}>
-              <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">{key}</h2>
-              <div className="space-y-1">
-                {route.children.map((child) => {
-                  const normalizedPathname = normalizePath(pathname);
-                  const normalizedRoute = normalizePath(child.route);
-
-                  return (
-                    <Button
-                      onClick={() => router.push(child.route)}
-                      key={child.key}
-                      variant={normalizedPathname === normalizedRoute ? "secondary" : "ghost"}
-                      className="w-full justify-start"
-                    >
-                      {child.name}
-                    </Button>
-                  );
-                })}
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <div className={cn("pb-12", className)}>
+                <div className="space-y-4 py-4">
+                  {Object.entries(routes).map(([key, route]) => (
+                    <div key={key}>
+                      <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">{key}</h2>
+                      <div className="space-y-1 ml-2">
+                        {route.children.map((child) => {
+                          const normalizedPathname = normalizePath(pathname);
+                          const normalizedRoute = normalizePath(child.route);
+                          return (
+                            <SidebarMenuButton
+                              onClick={() => router.push(child.route)}
+                              key={child.key}
+                              variant={
+                                normalizedPathname === normalizedRoute ? "outline" : "default"
+                              }
+                              className="w-full justify-start"
+                            >
+                              {child.icon}
+                              {child.name}
+                            </SidebarMenuButton>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
   );
 }
