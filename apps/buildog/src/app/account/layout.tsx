cf33abcf0da251e97391e09855d09b61e0f9c194
REDLANTERNDEV <87618221+REDLANTERNDEV@users.noreply.github.com>
2024-12-23 17:36:20 +0300

    feat(buildog): add sidebar components and mobile detection hook

--- apps/buildog/src/app/account/layout.tsx
+++ apps/buildog/src/app/account/layout.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Appbar from "@/components/app-bar";
+import { SidebarProvider } from "@ui/components/ui/sidebar";
 import { AccountSidebar } from "@/components/account-sidebar";
 
 export default function AccountLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex w-full">
-      <AccountSidebar className="w-[300px] border-r" />
-      <div className="flex flex-col w-full">
-        <Appbar />
-        <div className="flex-grow p-5 mx-auto overflow-auto w-full">{children}</div>
+    <SidebarProvider>
+      <div className="flex w-full">
+        <AccountSidebar />
+        <div className="flex flex-col w-full">
+          <Appbar />
+          <main className="flex-grow p-5 mx-auto overflow-auto w-full">{children}</main>
+        </div>
       </div>
-    </div>
+    </SidebarProvider>
   );
 }
--- apps/buildog/src/components/app-bar.tsx
+++ apps/buildog/src/components/app-bar.tsx
@@ -4,13 +4,17 @@ import React from "react";
 
 import OrgNavigation from "./org-navigation";
 import AvatarDropdown from "./avatar-dropdown";
+import { SidebarTrigger } from "@ui/components/ui/sidebar";
 
 const Appbar = () => {
   return (
     <header className="px-4 border-b">
-      <nav className="w-full flex items-center justify-between py-2">
+      <nav className="w-full flex items-center py-2">
+        <SidebarTrigger className="mr-4" />
         <OrgNavigation />
-        <AvatarDropdown />
+        <div className="ml-auto">
+          <AvatarDropdown />
+        </div>
       </nav>
     </header>
   );
