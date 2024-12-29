37a8e6641718876090720457f9b983f7819363b1
kursatcanciger <kursatcanciger@hotmail.com>
2024-12-29 15:24:42 +0300

    updated icons under packages/ui and removed @radix-ui/react-icons.

--- packages/ui/components/ui/command.tsx
+++ packages/ui/components/ui/command.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { type DialogProps } from "@radix-ui/react-dialog";
-import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { MagnifyingGlass } from "./react-icons";
 import { Command as CommandPrimitive } from "cmdk";
 
 import { cn } from "@ui/lib/utils";
@@ -42,7 +42,7 @@ const CommandInput = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input>
 >(({ className, ...props }, ref) => (
   <div className="flex items-center border-b px-3" cmdk-input-wrapper="">
-    <MagnifyingGlassIcon className="mr-2 h-4 w-4 shrink-0 opacity-50" />
+    <MagnifyingGlass className="mr-2 h-4 w-4 shrink-0 opacity-50" />
     <CommandPrimitive.Input
       ref={ref}
       className={cn(
