7901048ee3f538628d2c4c554a6951da89e42114
Kuzey Kose <kosekuzey97@gmail.com>
2025-07-02 22:08:51 -0400

    feat: init tiptap

---
+++ packages/editor/src/Tiptap.tsx
@@ -0,0 +1,25 @@
+// src/Tiptap.tsx
+import { useEditor, EditorContent, FloatingMenu, BubbleMenu } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+
+// define your extension array
+const extensions = [StarterKit];
+
+const content = "<p>Hello World!</p>";
+
+const Tiptap = () => {
+  const editor = useEditor({
+    extensions,
+    content,
+  });
+
+  return (
+    <>
+      <EditorContent editor={editor} />
+      <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>
+      <BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>
+    </>
+  );
+};
+
+export default Tiptap;
